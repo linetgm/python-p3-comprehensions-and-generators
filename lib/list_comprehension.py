53b7c3390b56d455b47ca38d72ec3aaca69fb03b
Linet <muthiigathoni@gmail.com>
2023-09-12 22:25:43 +0300

    Complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclaimed_sentences
